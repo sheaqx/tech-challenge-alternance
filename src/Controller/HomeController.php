7e7ab615eb859abf627b2c6e796c65a174818493
sheaqx <sheimanelabbar@gmail.com>
2022-12-14 23:45:51 +0100

    form works need refacto

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,19 +2,29 @@
 
 namespace App\Controller;
 
+use App\Entity\Argonaute;
+use App\Form\ArgonauteType;
 use App\Repository\ArgonauteRepository;
+// use Doctrine\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(ArgonauteRepository $argonauteRepository): Response
+    public function index(ArgonauteRepository $argonauteRepository, Request $request): Response
     {
         $argonautes = $argonauteRepository->findAll();
+        $newArgonaute = new Argonaute();
+        $form = $this->createForm(ArgonauteType::class, $newArgonaute);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $argonauteRepository->save($newArgonaute, true);
+        }
         return $this->render('home/index.html.twig', [
-            'argonautes' => $argonautes
+            'argonautes' => $argonautes, 'form' => $form
         ]);
     }
 }
---
+++ src/Form/ArgonauteType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Argonaute;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+
+class ArgonauteType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'required' => true,
+                'label' => 'Nom de l\'Argonaute'
+            ])
+            ->add('upload', SubmitType::class, [
+                'label' => 'Envoyer',
+                // 'attr' => ['class' => 'btn ']
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Argonaute::class,
+        ]);
+    }
+}
